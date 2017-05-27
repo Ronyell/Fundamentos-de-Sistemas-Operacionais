4b606358bc89523eec5137e8d63ee9ade3016f93
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-05-27 15:44:30 -0300

    Adding libseno.

--- Ronyell_Santos_150046073_Thiago_Freire_150047142_trab04/inc/libseno.h
+++ Ronyell_Santos_150046073_Thiago_Freire_150047142_trab04/inc/libseno.h
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PI 3.1415926536
+#define NUMERO_INTERACOES 10
+#define RADIANO 180
+#define SENO "-s"
+#define ARC_SENO "-a"
+
+
 double seno(double angulo);
 
 double arc_seno(double seno);
--- Ronyell_Santos_150046073_Thiago_Freire_150047142_trab04/src/libseno.c
+++ Ronyell_Santos_150046073_Thiago_Freire_150047142_trab04/src/libseno.c
@@ -4,7 +4,21 @@
 
 
 double seno(double angulo){
-  return 3.0 - 5.0;
+  int contador, numeroInteracoes;
+  double soma, termo;
+
+  //Número de interações para calcular a série, quanto maior menor o erro de seno.
+  numeroInteracoes = 10000;
+  termo = angulo;
+  soma = angulo;
+
+  /* Loop to calculate the value of Sine */
+  for(contador=1; contador <= numeroInteracoes; contador++)
+  {
+    termo = ( termo * ( -1 ) * angulo * angulo ) / (2 * contador * ( 2 * contador + 1 ) ) * 1.0;
+    soma = soma + termo;
+  }
+  return soma;
 }
 
 double arc_seno(double seno){
