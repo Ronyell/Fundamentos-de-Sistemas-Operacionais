d165a3549913e387998513eea5ddb73a4a20d4b8
Thiago Nogueira <thiagonf10@gmail.com>
2017-05-30 23:23:38 -0300

    Pegando os parâmetros do terminal.

--- arcseno.c
+++ arcseno.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-int main(){
+int main(int argc, char * argv[]){
   double resultado = 0;
-  double resultadoF = 0;
   double x, x1, x2, x3, x4, x5, x6;
+  char seno[] = "-s";
+  char arcseno[] = "-a";
+  int verificaSeno = 0, verificaArcseno = 0;
 
-  scanf("%lf", &x);
+  verificaSeno = strcmp(seno, argv[1]);
+  verificaArcseno = strcmp(arcseno, argv[1]);
+  x = atof(argv[2]);
 
-  x1 = x;
-  x2 = ((x*x*x)/3.0)*(1.0/2.0);
-  x3 = (1.0/2.0)*(3.0/4.0)*((x*x*x*x*x)/3.0);
-  x4 = (1.0/2.0)*(3.0/4.0)*(5.0/6.0)*((x*x*x*x*x*x*x)/3.0);
-  x5 = (1.0/2.0)*(3.0/4.0)*(5.0/6.0)*(7.0/8.0)*((x*x*x*x*x*x*x*x*x)/3.0);
-  //x6 = (1.0/2.0)*(3.0/4.0)*(5.0/6.0)*(7.0/8.0)*(9.0/10.0)*((x*x*x*x*x*x*x*x*x*x*x)/3.0);
+  if(verificaSeno == 0){
+    printf("Função Seno: \n");
+  } else if(verificaArcseno == 0){
+    printf("Função Arcseno: \n");
 
+    x1 = x;
+    x2 = ((x*x*x)/3.0)*(1.0/2.0);
+    x3 = (1.0/2.0)*(3.0/4.0)*((x*x*x*x*x)/3.0);
+    x4 = (1.0/2.0)*(3.0/4.0)*(5.0/6.0)*((x*x*x*x*x*x*x)/3.0);
+    x5 = (1.0/2.0)*(3.0/4.0)*(5.0/6.0)*(7.0/8.0)*((x*x*x*x*x*x*x*x*x)/3.0);
 
-  printf("x1: %lf\n", x1);
-  printf("x2: %lf\n", x2);
-  printf("x3: %lf\n", x3);
-  printf("x4: %lf\n", x4);
-  printf("x5: %lf\n", x5);
-  printf("x6: %lf\n", x6);
+    resultado = x1 + x2 + x3 + x4 + x5;
 
+    printf("Meu arco seno: %lf\n", resultado);
+    printf("Arco seno math.h: %lf\n", asin(x));
 
-  resultado = x1 + x2 + x3 + x4 + x5;
+  } else {
 
-  printf("Meu arco seno: %lf", resultado);
-  printf("Arco seno math.h: %lf", asin(x));
+  }
+
   return 0;
 }
